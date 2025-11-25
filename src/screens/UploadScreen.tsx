7e6accc86246a7267f6dd0ac8419eb6eff1d425d
Omar Swidan <oswidan306@gmail.com>
2025-11-24 16:52:39 -0800

    Increase splash font size, move button up, disable upload scrolling, add scrim, remove chef review text

--- src/screens/UploadScreen.tsx
+++ src/screens/UploadScreen.tsx
@@ -33,9 +33,6 @@ export function UploadScreen() {
             onFileSelect={(file, preview) => selectPlate(file, preview)}
           />
         </div>
-        <div className={`upload-screen__subheader ${isAnimating ? 'upload-screen__subheader--slide-down' : ''}`}>
-          <p className="upload-screen__subheader-text">FOR THE CHEF'S REVIEW</p>
-        </div>
         {platePreview && (
           <div className={`upload-screen__footer ${isAnimating ? 'upload-screen__footer--slide-down' : ''}`}>
             <button
